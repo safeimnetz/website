5dd1f597f25288ef618d66aaf4d49150272e3172
Kilian Prechtl <prekic19@htl-kaindorf.at>
2023-10-15 14:26:57 +0200

    Remade the complete header that it has a navbar and is responsive

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import {Inter} from 'next/font/google'
 import styles from "@/app/page.module.css";
 import Image from "next/image";
 import Link from 'next/link';
+import ImportBsJS from "@/app/ImportBsJS";
 
 
 const inter = Inter({subsets: ['latin']})
@@ -16,19 +17,41 @@ export default function RootLayout({children}) {
     return (
         <html lang="de">
             <body className={inter.className}>
+                <ImportBsJS />
                 <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
 
                     {/*Header*/}
-                    <div className={"container-fluid fixed-top"} style={{ height: "70px", background: "linear-gradient(45deg, #883D8C, #EF2DCE)"}}>
-                        <div className={styles.headerImage}>
-                            <Link href="http://localhost:3000">
-                                <Image src={"/Text_White.png"} alt={"Logo"} width={"245"} height={"70"}></Image>
+                    <nav className="navbar navbar-expand-lg fixed-top navbar-dark" style={{ background: "linear-gradient(45deg, #883D8C, #EF2DCE)"}}>
+                        <div className="container-fluid">
+                            <Link href="http://localhost:3000" className="navbar-brand">
+                                <Image src={"/Text_White.png"} alt={"Logo"} width={"245"} height={"70"} />
                             </Link>
+
+                            {/* Toggler for mobile view */}
+                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+
+                            <div className="collapse navbar-collapse" id="navbarNav">
+                                <ul className="navbar-nav ms-auto"> {/* ms-auto pushes items to the right */}
+                                    <li className="nav-item">
+                                        <Link href="http://localhost:3000/imprint" className="nav-link">Impressum</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link href="http://localhost:3000/terms" className="nav-link">Nutzungsbedingungen</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link href="http://localhost:3000/privacy" className="nav-link">Datenschutzerklärung</Link>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                    </div>
+                    </nav>
+
+
 
                     {/*Main body*/}
-                    <div className={"container flex-grow-1"} style={{marginTop: "85px"}}>
+                    <div className={"container flex-grow-1"} style={{marginTop: "105px"}}>
                         {children}
                     </div>
 
