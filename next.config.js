1fb22e3b91e32582276f8a76ec3c36a4a66607e7
Laurens Kropf <laurens.kropf@kropf-it.at>
2023-10-16 22:15:07 +0200

    trailing slashes options

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    distDir: 'build'
+    distDir: 'build',
+    trailingSlash: true,
 }
 
 module.exports = nextConfig
