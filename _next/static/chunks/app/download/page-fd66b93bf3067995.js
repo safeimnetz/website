b25b1dc73301fd44fb524dd8e249b2585a5398dd
Laurens Kropf <laurens.kropf@kropf-it.at>
2023-10-16 21:57:06 +0200

    Updates

---
+++ _next/static/chunks/app/download/page-fd66b93bf3067995.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[40],{8766:function(e,t,o){Promise.resolve().then(o.bind(o,6190))},6190:function(e,t,o){"use strict";o.r(t),o.d(t,{default:function(){return Download}});var n=o(7437),r=o(2265);function Download(){return(0,r.useEffect)(()=>{let e=navigator.userAgent||navigator.vendor;/android|Android/i.test(e)||navigator.userAgent.toLowerCase().indexOf("android")>-1?window.location.href="https://play.google.com/store/apps/details?id=at.safeimnetz.app":/iPad|iPhone|iPod/.test(e)||/Macintosh/.test(e)?window.location.href="http://localhost:3000":window.location.href="/"},[]),(0,n.jsx)("div",{})}},622:function(e,t,o){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var n=o(2265),r=Symbol.for("react.element"),i=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),a=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,s={key:!0,ref:!0,__self:!0,__source:!0};t.jsx=function(e,t,o){var n,f={},u=null,c=null;for(n in void 0!==o&&(u=""+o),void 0!==t.key&&(u=""+t.key),void 0!==t.ref&&(c=t.ref),t)i.call(t,n)&&!s.hasOwnProperty(n)&&(f[n]=t[n]);if(e&&e.defaultProps)for(n in t=e.defaultProps)void 0===f[n]&&(f[n]=t[n]);return{$$typeof:r,type:e,key:u,ref:c,props:f,_owner:a.current}}},7437:function(e,t,o){"use strict";e.exports=o(622)}},function(e){e.O(0,[971,864,744],function(){return e(e.s=8766)}),_N_E=e.O()}]);
